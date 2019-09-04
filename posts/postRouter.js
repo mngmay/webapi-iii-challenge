6a6b04cc961bf95a2423279b87de520dd565aec4
May Ng <mngmay@gmail.com>
2019-09-04 16:05:37 -0700

    created GET for posts and middleware for validating by post id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {});
+const Posts = require("../posts/postDb.js");
+
+//GET /posts
+router.get("/", (req, res) => {
+  Posts.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved" });
+    });
+});
 
 router.get("/:id", (req, res) => {});
 
@@ -10,6 +23,22 @@ router.put("/:id", (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+function validatePostId(req, res, next) {
+  let id = req.params.id;
+
+  Posts.getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+      } else {
+        res.status(400).json({ message: "invalid post id" });
+      }
+    })
+    .catch(error =>
+      res.status(500).json({ message: "The post could not be retrieved" })
+    );
+
+  next();
+}
 
 module.exports = router;
